e415f1f4a0e13a0b0744d837de4d63e6140360a1
DaPiankov <DaPiankov@croc.ru>
2024-02-15 16:12:25 +0300

    [feat]: Added base Spring Boot structure

---
+++ src/main/java/heavenboards/composer/service/ComposerServiceApplication.java
@@ -0,0 +1,11 @@
+package heavenboards.composer.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComposerServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ComposerServiceApplication.class, args);
+    }
+}
---
+++ src/test/java/heavenboards/composer/service/ComposerServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package heavenboards.composer.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComposerServiceApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
