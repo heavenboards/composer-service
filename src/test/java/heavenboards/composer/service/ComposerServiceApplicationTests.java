74a867cd15e1e8a7384b0e5cd632cf973a38e40f
Pyankov Daniil <109591434+pyankovdaniil@users.noreply.github.com>
2024-03-13 22:42:09 +0300

    Настроить деплой микросервисов (#8)
    * [feat]: Добавил деплой
    
    * [feat]: Добавил деплой 2
    
    * [feat]: Добавил профиль test
    
    * [fix]: Сделал джобы dockerize и deploy только при мердже в main
    
    * [fix]: Поправил пайплайны
    
    * [fix]: Поправил пайплайны
    
    ---------
    
    Co-authored-by: DaPiankov <DaPiankov@croc.ru>

--- src/test/java/heavenboards/composer/service/ComposerServiceApplicationTests.java
+++ src/test/java/heavenboards/composer/service/ComposerServiceApplicationTests.java
@@ -2,8 +2,10 @@ package heavenboards.composer.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ComposerServiceApplicationTests {
     @Test
     void contextLoads() {
